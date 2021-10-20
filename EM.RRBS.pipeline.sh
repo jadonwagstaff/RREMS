e6d6b26147075b79f5be0deacdf47c4e7dbb7d99
Jadon Wagstaff <jadonwagstaff@Jadon-Work.local>
2021-10-20 17:46:09 -0600

    Rewrite pipeline to fix top-bottom strand mismatch

--- EM.RRBS.pipeline.sh
+++ EM.RRBS.pipeline.sh
@@ -7,90 +7,94 @@ ml cutadapt
 ml trim_galore
 ml samtools
 
+from=$1
+to=$2
 
-
-r1=$1
-r2=$2
-r3=$3
-
-r1Base=`basename $r1`
-r2Base=`basename $r2`
-r3Base=`basename $r3`
-
-directory=`dirname $r1`
-cd $directory
-
-gunzip *.fastq.gz &
-wait
-
-r1UMI="${r1Base%.fastq.gz}.UMIAdded.fastq"
-r3UMI="${r3Base%.fastq.gz}.UMIAdded.fastq"
-
-sample=`basename $directory`
-
-echo "Adding Barcodes to Read Files"
-# This is a script Ryan wrote and Mark modified to add the barcode (R2) to the header of the FastQ read files (R1 & R3)
-python /uufs/chpc.utah.edu/common/home/varley-group2/FastqFiles/MarkWadsworth/scripts/addBarcodes_novaseq_variableSize.py -d ${r1Base%.gz} -b ${r2Base%.gz} -o $r1UMI -s $sample -n 12 &
-python /uufs/chpc.utah.edu/common/home/varley-group2/FastqFiles/MarkWadsworth/scripts/addBarcodes_novaseq_variableSize.py -d ${r3Base%.gz} -b ${r2Base%.gz} -o $r3UMI -s $sample -n 12 &
-
-wait
-
-gzip *.fastq
-
-r1UMI=`ls *R1*.UMIAdded.fastq.gz`
-r3UMI=`ls *R3*.UMIAdded.fastq.gz`
-
-
-echo $r1UMI
-echo $r3UMI
-
-echo "Run FastQC and Trim Reads"
-## Runs FastQC on the original files
-fastqc $r1UMI &
-fastqc $r2Base &
-fastqc $r3UMI &
-
-## Trims the adapters off the reads. --rrbs is used here because we use the MspI cutsites and it controls for that.
-trim_galore --rrbs --fastqc --paired $r1UMI $r3UMI &
-
-wait
-
-r1="${r1UMI%.fastq.gz}_val_1.fq.gz"
-r3="${r3UMI%.fastq.gz}_val_2.fq.gz"
-
-echo $r1
-echo $r3
-
+samples=$(ls $from | awk -F_ '{print $1}' | uniq)
 REFPATH="/uufs/chpc.utah.edu/common/home/varley-group2/ReferenceGenomes/RyanMiller/hg19"
 
-echo "Bismark Alignment Started"
-## Bismark alignment using Bowtie2
-bismark --multicore 6 --bowtie2 --un --ambiguous -N 1 --temp_dir TempDelme --non_bs_mm $REFPATH -1 $r1 -2 $r3 --quiet -o ./bowtie2.trimmed/
-echo "Bismark Alignment Finished"
+while IFS= read -r sample
+do
+    # Uncompress raw data
+    gunzip "$from/$sample"*.fastq.gz
 
 
-cd bowtie2.trimmed/
+    ###   Add Barcodes   ###
+    echo "====================================================="
+    echo "ADDING BARCODES FOR " $sample
+    echo "====================================================="
+    echo ""
+    python addBarcodes_novaseq_variableSize.py -d "$from/$sample"_*_R1_* -b "$from/$sample"_*_R2_* -o "$to/$sample"_R1.fastq -s $sample -n 12 &
+    python addBarcodes_novaseq_variableSize.py -d "$from/$sample"_*_R3_* -b "$from/$sample"_*_R2_* -o "$to/$sample"_R3.fastq -s $sample -n 12 &
+    wait
 
-bowtie2Bam=`ls *bismark_bt2_pe.bam`
-## Extracts the CpG sites with their coverage
-echo "Bismark Methylation extractor and report started"
-bismark_methylation_extractor --bedgraph --zero_based -p $bowtie2Bam > $bowtie2Bam".methylationExtractor"
-## Outputs a nice HTML report of the run
-bismark2report
+    # Re-compress raw data
+    gzip "$from/$sample"*.fastq
 
-#gunzip *.cov.gz
+    # Make variables
+    r1="$to/$sample"_R1
+    r3="$to/$sample"_R3
 
-#zeroCovFile=`ls *zero.cov`
-#
-#echo "Creating BigBed"
-### This is a wrapper script Mark Wadsworth wrote that calls another script he wrote that turns the coverage file to a color coded  bigbed file for the UCSC Genome Browser track.
-#bash /uufs/chpc.utah.edu/common/home/varley-group2/FastqFiles/MarkWadsworth/scripts/runBismarkColorBed.sh -f $zeroCovFile -d 10
-#
-#echo "Creating 10x coverage file"
-### Creates a file of thee CpG to the percent methylation. This file will be merged with the other samples from the run.
-#awk -F"\t" '{if($5+$6 >= 10) print $1" "$2"\t"$4}' $zeroCovFile > "${zeroCovFile}.cpgs.moreThan10XCov.txt"
 
-cd ../
+    ###   Trim Adapters   ###
+    echo "====================================================="
+    echo "TRIMMING ADAPTERS FOR " $sample "\n\n\n"
+    echo "====================================================="
+    trim_galore --output_dir $to --rrbs --fastqc --paired "$r1".fastq "$r3".fastq
+
+    # Clean up output
+    mv "$r1"_val_1.fq "$r1".fastq &
+    mv "$r3"_val_2.fq "$r3".fastq &
+    mv "$r1"_val_1_fastqc.html "$r1".fastq_fastqc_report.html &
+    mv "$r3"_val_2_fastqc.html "$r3".fastq_fastqc_report.html &
+    rm "$r1"_val_1_fastqc.zip "$r3"_val_2_fastqc.zip &
+    wait
+
+
+    ###   Align Reads   ###
+    echo "====================================================="
+    echo "ALIGNING READS FOR " $sample
+    echo "====================================================="
+    echo ""
+    bismark --quiet --output $to --multicore 6 --bowtie2 --un --ambiguous -N 1 --temp_dir TempDelme --non_bs_mm $REFPATH -1 "$r1".fastq -2 "$r3".fastq
+    
+    # Clean up output
+    mv "$r1"_bismark_bt2_PE_report.txt "$to/$sample".bam_bismark_report.txt
+    mv "$r1"_bismark_bt2_pe.bam "$to/$sample".bam
+
+    # Compress clean data
+    gzip "$to/$sample"*.fastq
+
+
+    ###   Extract Methylation Data   ###
+    echo "====================================================="
+    echo "EXTRACTING MMETHYLATION DATA FOR " $sample
+    echo "====================================================="
+    echo ""
+    bismark_methylation_extractor --output $to --multicore 6 --paired-end "$to/$sample".bam
+    
+    # Clean up output
+    mv "$to/$sample"_splitting_report.txt "$to/$sample".bam_splitting_report.txt &
+    mv "$to/$sample".M-bias.txt "$to/$sample".bam_M-bias.txt &
+    awk '{if (NR > 1) {print $3, $4 - 1, $5}}' "$to"/CpG_OB_"$sample".txt > "$to/$sample"_OB.txt &
+    awk '{if (NR > 1) {print $3, $4, $5}}' "$to"/CpG_OT_"$sample".txt > "$to/$sample"_OT.txt &
+    wait
+    
+    rm "$to"/CHG_*_"$sample".txt &
+    rm "$to"/CHH_*_"$sample".txt &
+    rm "$to"/CpG_*_"$sample".txt &
+    wait
+    
+    # Combine top and bottom strand then count number of methylated and unmethylated
+    cat "$to/$sample"_OB.txt "$to/$sample"_OT.txt |
+    awk '{if (NR != 1) a[$0]++ } END{for (x in a) print x, a[x]}' |
+    awk '{if ($3 == "Z") print $1, $2, $4, 0; else print $1, $2, 0, $4}' |
+    awk '{a[$1 " " $2] += $3; b[$1 " " $2] += $4} END{for (x in a) print x, a[x], b[x]}' |
+    awk 'BEGIN{OFS = "\t"; print "Chromosome", "Location", "Methylated", "Unmethylated", "MethylP"} {$1 = $1; if ($3 + $4 >= 10) print $0, $3 / ($3 + $4)}' > "$to/$sample"_methylation.txt
+    
+    rm "$to/$sample"_OB.txt "$to/$sample"_OT.txt
+
+done <<< "$samples"
 
 
 
